4e85ce30e8c09cd68c0ae3c8603ecfe3d352b273
p-martin <p_martin@gmx.net>
2017-07-26 21:23:56 +0200

    test.html with slide-down dummy menu and fade-out/fade-in animation when using the menu.

--- tools.js
+++ tools.js
@@ -8,9 +8,9 @@ function getQueryParametersDict() {
 
 /**
  * Decodes a given string and returns the key value pairs.
- * @param {string} s source string to be decoded
- * @param {string} pairSep separator string between the pairs, defaults to semicolon (";")
- * @param {string} keyValueSep separator string between key and value for each pair, defaults to equal sign ("=")
+ * @param {String} s source string to be decoded
+ * @param {String} pairSep separator string between the pairs, defaults to semicolon (";")
+ * @param {String} keyValueSep separator string between key and value for each pair, defaults to equal sign ("=")
  * @return {object} dictionary (associative array) of key value pairs
  */
 function stringToDict(s, pairSep=";", keyValueSep="=") {
@@ -26,7 +26,7 @@ function stringToDict(s, pairSep=";", keyValueSep="=") {
 
 /**
  * Appends a new link tag to the HTML head, specifying the given stylesheet file.
- * @param {string} path path of the file, including the extension
+ * @param {String} path path of the file, including the extension
  */
 function appendCssFileToHead(path) {
 	var linkElement = document.createElement("link");
@@ -34,4 +34,27 @@ function appendCssFileToHead(path) {
 	linkElement.type = "text/css";
 	linkElement.href = path;
 	document.getElementsByTagName("head")[0].appendChild(linkElement);
+}
+
+/**
+ * Returns the "absolute" position (relative to the body) of the given element.
+ * @param element as determined e.g. via document.getElementById()
+ * @return {Array} left and top coordinates as an array's elements
+ */
+function getElementPosition(element) {
+	var [left, top] = [0, 0];
+	for (el = element; el; el = el.offsetParent) {
+		[left, top] = [left + el.offsetLeft, top + el.offsetTop];
+	}
+	return [left, top];
+}
+
+/**
+ * Returns the "absolute" bounds (relative to the body) of the given element.
+ * @param element as determined e.g. via document.getElementById()
+ * @return {object} bounds as the object's left, top, width, height, right, and bottom properties
+ */
+function getElementBounds(element) {
+	var [left, top] = getElementPosition(element);
+	return {left: left, top: top, width: element.offsetWidth, height: element.offsetHeight, right: left+element.offsetWidth, bottom: top+element.offsetHeight};
 }
